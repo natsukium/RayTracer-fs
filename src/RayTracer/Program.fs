fcaf8cd7acd0bc6765718868b1686386c9c435cc
natsukium <tomoya.otabi@gmail.com>
2021-08-15 17:11:14 +0900

    refactor: Set an entrypoint

--- src/RayTracer/Program.fs
+++ src/RayTracer/Program.fs
@@ -6,21 +6,24 @@ let ImageWidth = 256
 [<Literal>]
 let ImageHeight = 256
 
-sprintf "P3\n%d %d\n255" ImageWidth ImageHeight
-|> printfn "%s"
+[<EntryPoint>]
+let main _ =
+    sprintf "P3\n%d %d\n255" ImageWidth ImageHeight
+    |> printfn "%s"
 
-seq {
-    for j in List.rev [ 0 .. ImageHeight - 1 ] do
-        for i in [ 0 .. ImageWidth - 1 ] -> i, j
-}
-|> Seq.mapi
-    (fun count (i, j) ->
-        if count % ImageWidth = 0 then
-            eprintf "\rScanlines remaining: %d" (ImageHeight - 1 - (count / ImageWidth))
+    seq {
+        for j in List.rev [ 0 .. ImageHeight - 1 ] do
+            for i in [ 0 .. ImageWidth - 1 ] -> i, j
+    }
+    |> Seq.mapi
+        (fun count (i, j) ->
+            if count % ImageWidth = 0 then
+                eprintf "\rScanlines remaining: %d" (ImageHeight - 1 - (count / ImageWidth))
 
-        Color.init (float i / float (ImageWidth - 1)) (float j / float (ImageHeight - 1)) 0.25
-        |> Color.writeColor)
-|> String.concat ""
-|> printfn "%s"
+            Color.init (float i / float (ImageWidth - 1)) (float j / float (ImageHeight - 1)) 0.25
+            |> Color.writeColor)
+    |> String.concat ""
+    |> printfn "%s"
 
-eprintfn "\ndone"
+    eprintfn "\ndone"
+    0
