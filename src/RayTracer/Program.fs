0f7a713bb372b562dfdb2aeaefeaaffd967889b8
natsukium <tomoya.otabi@gmail.com>
2021-08-19 09:23:16 +0900

    feat: Final scene

--- src/RayTracer/Program.fs
+++ src/RayTracer/Program.fs
@@ -16,43 +16,77 @@ let rec rayColor world depth ray : Color =
             (1.0 - t) * Color.init 1.0 1.0 1.0
             + t * Color.init 0.5 0.7 1.0
 
-let aspectRatio = 16.0 / 9.0
+let aspectRatio = 3.0 / 2.0
 
 [<Literal>]
-let ImageWidth = 400
+let ImageWidth = 1200
 
 let ImageHeight = float ImageWidth / aspectRatio |> int
 
 [<Literal>]
-let SamplesPerPixel = 100
+let SamplesPerPixel = 500
 
 [<Literal>]
-let MaxDepth = 10
+let MaxDepth = 50
 
 module Scene =
-    let materialGround = Lambertian(Color.init 0.8 0.8 0.0)
-    let materialCenter = Lambertian(Color.init 0.1 0.2 0.5)
-    let materialLeft = Dielectric 1.5
-    let materialRight = Metal(Color.init 0.8 0.6 0.2, 0.0)
+    let randomScene () =
+        let materialGround = Lambertian(Color.init 0.5 0.5 0.5)
 
-    let world =
-        [ Sphere.init (Vec3.init 0.0 -100.5 -1.0) 100.0 materialGround
-          Sphere.init (Vec3.init 0.0 0.0 -1.0) 0.5 materialCenter
-          Sphere.init (Vec3.init -1.0 0.0 -1.0) 0.5 materialLeft
-          Sphere.init (Vec3.init -1.0 0.0 -1.0) -0.45 materialLeft
-          Sphere.init (Vec3.init 1.0 0.0 -1.0) 0.5 materialRight ]
+        let randomSphere (a, b) =
+            let chooseMat = random 0.0 1.0
+
+            let center =
+                Vec3.init (a + 0.9 * random 0.0 1.0) 0.2 (b + 0.9 * random 0.0 1.0)
+
+            if Vec3.length (center - Vec3.init 4.0 0.2 0.0) > 0.9 then
+                match chooseMat with
+                | diffuse when diffuse < 0.8 ->
+                    let albedo =
+                        Vec3.random 0.0 1.0 * Vec3.random 0.0 1.0
+
+                    let material = Lambertian albedo
+                    Some(Sphere.init center 0.2 material)
+                | metal when metal < 0.95 ->
+                    let albedo = Vec3.random 0.5 1.0
+                    let fuzz = random 0.0 0.5
+                    let material = Metal(albedo, fuzz)
+                    Some(Sphere.init center 0.2 material)
+                | _ ->
+                    let material = Dielectric 1.5
+                    Some(Sphere.init center 0.2 material)
+            else
+                None
+
+        let spheres =
+            [ for a in [ -11 .. 10 ] do
+                  for b in [ -11 .. 10 ] -> float a, float b ]
+            |> List.map randomSphere
+            |> List.filter Option.isSome
+            |> List.map Option.get
+
+        let material1 = Dielectric 1.5
+        let material2 = Lambertian(Color.init 0.4 0.2 0.1)
+        let material3 = Metal(Color.init 0.7 0.6 0.5, 0.0)
+
+        [ Sphere.init (Vec3.init 0.0 -1000.0 -0.0) 1000.0 materialGround
+          Sphere.init (Vec3.init 0.0 1.0 0.0) 1.0 material1
+          Sphere.init (Vec3.init -4.0 1.0 0.0) 1.0 material2
+          Sphere.init (Vec3.init 4.0 1.0 0.0) 1.0 material3 ]
+        @ spheres
+
 
 module Camera =
-    let lookFrom = Vec3.init 3.0 3.0 2.0
-    let lookAt = Vec3.init 0.0 0.0 -1.0
+    let lookFrom = Vec3.init 13.0 2.0 3.0
+    let lookAt = Vec3.init 0.0 0.0 0.0
     let vUp = Vec3.init 0.0 1.0 0.0
-    let distToFocus = lookFrom - lookAt |> Vec3.length
-    let aperture = 2.0
+    let distToFocus = 10.0
+    let aperture = 0.1
 
     let camera =
         Camera.init lookFrom lookAt vUp 20.0 aspectRatio aperture distToFocus
 
-let render (w, h) =
+let render scene (w, h) =
     [ 0 .. SamplesPerPixel - 1 ]
     |> List.fold
         (fun acc _ ->
@@ -65,7 +99,7 @@ let render (w, h) =
                 / float (ImageHeight - 1)
 
             acc
-            + (rayColor Scene.world MaxDepth
+            + (rayColor scene MaxDepth
                <| Camera.getRay u v Camera.camera))
         (Color.init 0.0 0.0 0.0)
 
@@ -73,6 +107,8 @@ let render (w, h) =
 
 [<EntryPoint>]
 let main _ =
+    let scene = Scene.randomScene ()
+
     sprintf "P3\n%d %d\n255" ImageWidth ImageHeight
     |> printfn "%s"
 
@@ -83,7 +119,7 @@ let main _ =
             if count % ImageWidth = 0 then
                 eprintf "\rScanlines remaining: %d" (ImageHeight - 1 - (count / ImageWidth))
 
-            render (i, j))
+            render scene (i, j))
     |> String.concat ""
     |> printfn "%s"
 
